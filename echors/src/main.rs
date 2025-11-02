668a4938af0ca5e4515a2a8bfde20e131b578558
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2025-11-02 15:22:02 +0530

    feat: echors done

--- echors/src/main.rs
+++ echors/src/main.rs
@@ -3,23 +3,22 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 /// Rust Version of Echo
-struct Args{
+struct Args {
     /// Input Text
     #[arg(required(true))]
     text: Vec<String>,
 
     /// Do not print Newline
     #[arg(short('n'))]
-    omit_newline: bool
+    omit_newline: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!(
+    print!(
         "{}{}",
         args.text.join(" "),
         if args.omit_newline { "" } else { "\n" }
-
     );
 }
